a43ab7ad9c2619104025bfe1dbf0f94b65e09b79
邓华锋 <denghuafeng@gmail.com>
2026-03-31 06:37:25 +0800

    🚚  refactor(ruoyi-demo):  包名改为org.itrys.demo

---
+++ ruoyi-modules/ruoyi-demo/src/main/java/org/itrys/demo/domain/package-info.java
@@ -0,0 +1,1 @@
+package org.itrys.demo.domain;
---
+++ ruoyi-modules/ruoyi-demo/src/main/java/org/itrys/demo/mapper/TestDemoEncryptMapper.java
@@ -0,0 +1,13 @@
+package org.itrys.demo.mapper;
+
+import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
+import org.itrys.demo.domain.TestDemoEncrypt;
+
+/**
+ * 测试加密功能
+ *
+ * @author Lion Li
+ */
+public interface TestDemoEncryptMapper extends BaseMapperPlus<TestDemoEncrypt, TestDemoEncrypt> {
+
+}
